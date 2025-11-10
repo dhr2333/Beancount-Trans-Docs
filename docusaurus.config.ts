854b6500e17b098901b9e24c8e471209c5478a06
daihaorui <Dai_Haorui@163.com>
2025-11-10 16:40:18 +0800

    feat(Docs): 与主项目集成实现CHANGLOG记录

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,6 @@ const config: Config = {
   projectName: 'Beancount-Trans', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -114,6 +113,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
 
   themes: ['@docusaurus/theme-mermaid'],
